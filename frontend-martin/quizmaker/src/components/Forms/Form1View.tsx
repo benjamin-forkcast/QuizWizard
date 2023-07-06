258bd0b2789a33a7fed3bf1b9d187609a3dde108
Martin Lindefors <lindefor@kth.se>
2023-07-06 12:54:04 +0200

    Fixed better navigation buttons and started on implementing actual form questions

--- frontend-martin/quizmaker/src/components/Forms/Form1View.tsx
+++ frontend-martin/quizmaker/src/components/Forms/Form1View.tsx
@@ -3,23 +3,26 @@ import { NavLink } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { useState } from 'react';
 
+import TextField from '@mui/material/TextField';
+import Slider from '@mui/material/Slider';
+
 export default function Form1View() {
   
 
   const [isVisible, setIsVisible] = useState(false);
+  const [numberOfQuestions, setNumberOfQuestions] = useState(1);
 
-  function handleNext() {
-    console.log('Moving to form 2')
-  }
-
-  function toggle() {
-    setIsVisible(!isVisible);
-  }
 
   return (
+    <>
       <div>
-        Form 1 <br /> Blabla <br/> Blabla <br /> Blabla <br/> Blabla
+        Number of Questions
       </div>
+      <div>
+        <TextField id="standard-basic" value={numberOfQuestions} label="" variant="standard" type="Number" onChange={(event: React.ChangeEvent<HTMLInputElement>) => {setNumberOfQuestions(Number(event.target.value));}}/>
+        <Slider defaultValue={numberOfQuestions} step={1} marks min={1} max={20} onChange={(event: Event, value) => {setNumberOfQuestions(Number(value));}}/>
+      </div>
+    </>
   );
 }
 
--- frontend-martin/quizmaker/src/components/Forms/FormPresenter.tsx
+++ frontend-martin/quizmaker/src/components/Forms/FormPresenter.tsx
@@ -6,21 +6,75 @@ import Form1View from "./Form1View";
 import Form2View from "./Form2View";
 import Form3View from "./Form3View";
 
+import IconButton from '@mui/material/IconButton';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
+import RadioButtonCheckedIcon from '@mui/icons-material/RadioButtonChecked';
+
 export default function FormPresenter() {
   const [form1IsVisible, setForm1IsVisible] = useState(true);
   const [form2IsVisible, setForm2IsVisible] = useState(false);
   const [form3IsVisible, setForm3IsVisible] = useState(false);
+  const [exitAnimation, setExitAnimation] = useState(-300);
+  const [currentForm, setCurrentForm] = useState(1);
+  // let exitAnimation : number = -300;
 
-  function toggle() {
+  function toggleForward() {
+    setExitAnimation(300);
     if (form1IsVisible) {
       setForm1IsVisible(false);
       setForm2IsVisible(true);
+      setCurrentForm(2);
     } else if (form2IsVisible) {
       setForm2IsVisible(false);
       setForm3IsVisible(true);
+      setCurrentForm(3);
     } else if (form3IsVisible) {
       setForm3IsVisible(false);
       setForm1IsVisible(true);
+      setCurrentForm(1);
+    }
+  }
+
+  function toggleBackward() {
+    setExitAnimation(-300);
+    if (form1IsVisible) {
+      setForm1IsVisible(false);
+      setForm3IsVisible(true);
+      setCurrentForm(3);
+    } else if (form2IsVisible) {
+      setForm2IsVisible(false);
+      setForm1IsVisible(true);
+      setCurrentForm(1);
+    } else if (form3IsVisible) {
+      setForm3IsVisible(false);
+      setForm2IsVisible(true);
+      setCurrentForm(2);
+    }
+  }
+
+  function toggleForm(formNumber: number) {
+    if (currentForm > formNumber) {
+      setExitAnimation(-300);
+    } else {
+      setExitAnimation(300);
+    }
+    if (formNumber === 1) {
+      setForm1IsVisible(true);
+      setForm2IsVisible(false);
+      setForm3IsVisible(false);
+      setCurrentForm(1);
+    } else if (formNumber === 2) {
+      setForm1IsVisible(false);
+      setForm2IsVisible(true);
+      setForm3IsVisible(false);
+      setCurrentForm(2);
+    } else if (formNumber === 3) {
+      setForm1IsVisible(false);
+      setForm2IsVisible(false);
+      setForm3IsVisible(true);
+      setCurrentForm(3);
     }
   }
 
@@ -34,9 +88,9 @@ export default function FormPresenter() {
           {form1IsVisible && (
             <motion.div
               key={1}
-              initial={{ x: 300, opacity: 0 }}
+              initial={{ x: exitAnimation, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              exit={{ x: -300, opacity: 0 }}
+              exit={{ opacity: 0 }}
               className="questions"
             >
               <Form1View />
@@ -45,9 +99,9 @@ export default function FormPresenter() {
           {form2IsVisible && (
             <motion.div
               key={2}
-              initial={{ x: 300, opacity: 0 }}
+              initial={{ x: exitAnimation, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              exit={{ x: -300, opacity: 0 }}
+              exit={{ opacity: 0 }}
               className="questions"
             >
               <Form2View />
@@ -56,9 +110,9 @@ export default function FormPresenter() {
           {form3IsVisible && (
             <motion.div
               key={3}
-              initial={{ x: 300, opacity: 0 }}
+              initial={{ x: exitAnimation, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              exit={{ x: -300, opacity: 0 }}
+              exit={{ opacity: 0 }}
               className="questions"
             >
               <Form3View />
@@ -67,7 +121,11 @@ export default function FormPresenter() {
         </AnimatePresence>
       </div>
       <div className="navigate">
-        <button onClick={toggle}>Next</button>
+        <IconButton aria-label="navigate-left"><ChevronLeftIcon onClick={toggleBackward}/></IconButton>
+        <IconButton aria-label="navigate-1st">{currentForm===1&&<RadioButtonCheckedIcon/> || <RadioButtonUncheckedIcon onClick={(event) => toggleForm(1)}/>}</IconButton>
+        <IconButton aria-label="navigate-2nd">{currentForm===2&&<RadioButtonCheckedIcon/> || <RadioButtonUncheckedIcon onClick={(event) => toggleForm(2)}/>}</IconButton>
+        <IconButton aria-label="navigate-3rd">{currentForm===3&&<RadioButtonCheckedIcon/> || <RadioButtonUncheckedIcon onClick={(event) => toggleForm(3)}/>}</IconButton>
+        <IconButton aria-label="navigate-right"><ChevronRightIcon onClick={toggleForward}/></IconButton>
       </div>
     </div>
   );
